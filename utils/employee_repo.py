6adfefe9457e6995ee1bb2ce2d93f718a974fd51
chaelim <dlcofla414@naver.com>
2026-02-23 21:10:06 +0900

    feat: update 추가

--- utils/employee_repo.py
+++ utils/employee_repo.py
@@ -26,6 +26,28 @@ def insert_employee(conn, values):
 
     cursor.execute(query, values)
     conn.commit()
+    return cursor.lastrowid
+
+def update_employee(conn, emp_id, values):
+    cursor = conn.cursor()
+
+    query = """
+    UPDATE employees
+    SET name=%s, age=%s, business_travel=%s, department=%s,
+        distance_from_home=%s, education=%s, education_field=%s,
+        environment_satisfaction=%s, gender=%s, job_involvement=%s,
+        job_level=%s, job_satisfaction=%s, marital_status=%s,
+        monthly_income=%s, num_companies_worked=%s, overtime=%s,
+        percent_salary_hike=%s, performance_rating=%s,
+        relationship_satisfaction=%s, total_working_years=%s,
+        work_life_balance=%s, years_at_company=%s,
+        years_in_current_role=%s, years_since_last_promotion=%s,
+        job_role=%s
+    WHERE emp_id=%s
+    """
+
+    cursor.execute(query, (*values, emp_id))
+    conn.commit()
 
 def update_attrition(conn, emp_id, prob):
     cursor = conn.cursor()
