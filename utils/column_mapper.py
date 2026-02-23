25d090cc4354392e4f24653e1ff40b7feec23c33
chaelim <dlcofla414@naver.com>
2026-02-23 17:39:38 +0900

    feat: mapper 추가

---
+++ utils/column_mapper.py
@@ -0,0 +1,31 @@
+# 한국어 -> 영어
+KOR_TO_ENG = {
+    "이름": "name",
+    "나이": "age",
+    "출장빈도": "business_travel",
+    "부서": "department",
+    "집과의거리": "distance_from_home",
+    "교육수준": "education",
+    "전공분야": "education_field",
+    "근무환경만족도": "environment_satisfaction",
+    "성별": "gender",
+    "직무몰입도": "job_involvement",
+    "직급": "job_level",
+    "직무만족도": "job_satisfaction",
+    "결혼상태": "marital_status",
+    "월급": "monthly_income",
+    "이전회사근무횟수": "num_companies_worked",
+    "초과근무여부": "overtime",
+    "급여인상률": "percent_salary_hike",
+    "성과평가등급": "performance_rating",
+    "대인관계만족도": "relationship_satisfaction",
+    "총경력년수": "total_working_years",
+    "워라밸수준": "work_life_balance",
+    "현회사근속년수": "years_at_company",
+    "현재직무근무년수": "years_in_current_role",
+    "마지막승진후경과년수": "years_since_last_promotion",
+    "직무분류": "job_role"
+}
+
+# 영어-> 한국어 
+ENG_TO_KOR = {v: k for k, v in KOR_TO_ENG.items()}
